6c81fd6cdb317a6b474b24771c383a115bc593a2
nagacharankumarreddy@gmail.com <nagacharankumarreddy@gmail.com>
2021-05-10 15:17:25 +0530

    adjusted dropdown

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,21 +4,21 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function Navbar(){
     return(
 
-    <nav  class="navbar navbar-expand-sm navbar-dark bg-dark mb-0">
-        <div class="container">
-            <a class="navbar-brand" href="#">Book Now</a>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+    <nav  className="navbar navbar-expand-sm navbar-dark bg-dark mb-0">
+        <div className="container">
+            <a className="navbar-brand" href="#">Book Now</a>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Contact Us</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Map</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Map</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">INR</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">INR</a>
                 </li>
             </ul>
         </div>
